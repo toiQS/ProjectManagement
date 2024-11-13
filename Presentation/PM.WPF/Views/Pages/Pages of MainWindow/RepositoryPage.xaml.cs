b0dad314e367a1a23b21ca769127fe57f76dea76
Akai <nguyensieu12112002@gmail.com>
2024-11-13 23:32:50 +0700

    up

---
+++ Presentation/PM.WPF/Views/Pages/Pages of MainWindow/RepositoryPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using PM.WPF.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PM.WPF.Views.Pages.Pages_of_MainWindow
+{
+    /// <summary>
+    /// Interaction logic for RepositoryPage.xaml
+    /// </summary>
+    public partial class RepositoryPage : Page
+    {
+        public RepositoryPage()
+        {
+            InitializeComponent();
+            this.DataContext = new HomePageViewModel();
+        }
+
+        private void ProjectListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
