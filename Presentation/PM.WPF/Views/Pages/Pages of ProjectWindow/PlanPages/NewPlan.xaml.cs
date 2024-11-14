9ccb16b219c084dee92b0d5728ec0d5b521f8af2
Akai <nguyensieu12112002@gmail.com>
2024-11-14 21:04:40 +0700

    up

---
+++ Presentation/PM.WPF/Views/Pages/Pages of ProjectWindow/PlanPages/NewPlan.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PM.WPF.Views.Pages.Pages_of_ProjectWindow.PlanPages
+{
+    /// <summary>
+    /// Interaction logic for NewPlan.xaml
+    /// </summary>
+    public partial class NewPlan : Window
+    {
+        public NewPlan()
+        {
+            InitializeComponent();
+        }
+    }
+}
