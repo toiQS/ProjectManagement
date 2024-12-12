c3160bff278f503fe74b6c4295e3d586924fd524
Akai <nguyensieu12112002@gmail.com>
2024-12-12 07:58:09 +0700

    Revert "del"
    This reverts commit f14957b144ea2dcd9ec65fccf340ecfec31b6ac1.

---
+++ Presentation/PM.WPF/Views/Pages/Pages of ProjectWindow/PlanManagementPage.xaml.cs
@@ -0,0 +1,49 @@
+﻿using PM.WPF.Models.plan;
+using PM.WPF.ViewModels;
+using PM.WPF.Views.Windows;
+using System;
+using System.Collections.Generic;
+using System.DirectoryServices.ActiveDirectory;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Wpf.Ui.Controls;
+
+namespace PM.WPF.Views.Pages.Pages_of_ProjectWindow
+{
+    /// <summary>
+    /// Interaction logic for PlanManagementPage.xaml
+    /// </summary>
+    public partial class PlanManagementPage : Page
+    {
+        public PlanManagementPage()
+        {
+            InitializeComponent();
+
+            DataContext = new PlanViewModel();
+            AddContextConponent();
+        }
+        public void AddContextConponent()
+        {
+            PlanNameText.Text = "Member";
+        }
+
+        private void PlanItemListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if(PlanItemListView.SelectedItem is PlanItem planItem)
+            {
+                TaskWindow taskWindow = new TaskWindow();
+                taskWindow.Show();
+            }
+        }
+    }
+}
---
+++ Presentation/PM.WPF/Views/Pages/Pages of ProjectWindow/PlanPages/InfoDetailPlan.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PM.WPF.Views.Pages.Pages_of_ProjectWindow.PlanPages
+{
+    /// <summary>
+    /// Interaction logic for InfoDetailPlan.xaml
+    /// </summary>
+    public partial class InfoDetailPlan : Window
+    {
+        public InfoDetailPlan()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCloseClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
