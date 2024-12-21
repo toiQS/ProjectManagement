78cae622a8eac32b5d244c9f0f4162e8d38e5b09
Akai <nguyensieu12112002@gmail.com>
2024-12-21 15:44:13 +0700

    delete weather forecast model and controller

---
+++ Presentation/PM.API/Controllers/AuthController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Shared.appUser;
+
+namespace PM.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        //private string _authPath = "http://localhost:5298/api/auth/";
+        //private readonly ILogger _logger;
+        //private readonly HttpClient _httpClient;
+        //public AuthController(ILogger<AuthController> logger)
+        //{
+        //    _httpClient = new HttpClient();
+        //    _logger = logger;
+        //    _httpClient.BaseAddress = new Uri(_authPath);
+        //}
+        //public async Task Login(string email, string password)
+        //{
+        //    var model = new LoginModel()
+        //    {
+        //        Email = email,
+        //        Password = password
+        //    };
+        //    await _httpClient.PostAsJsonAsync<LoginModel>("login", model);
+        //}
+    }
+}
