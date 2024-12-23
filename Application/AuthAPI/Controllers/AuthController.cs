238aa752dd06b0753f27ddaa324177cc7d50cdad
Akai <nguyensieu12112002@gmail.com>
2024-12-23 22:48:02 +0700

    up

---
+++ Application/AuthAPI/Controllers/AuthController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PM.DomainServices.ILogic;
+using PM.Infrastructure.Jwts;
+using PM.Infrastructure.Loggers;
+using Shared.appUser;
+
+namespace AuthAPI.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthLogic _auth;
+        private readonly IJwtHelper _jwtHelper;
+        private readonly ILoggerHelper<AuthController> _loggerHelper;
+        public AuthController(IAuthLogic auth, IJwtHelper jwtHelper, ILoggerHelper<AuthController> loggerHelper)
+        {
+            _auth = auth;
+            _jwtHelper = jwtHelper;
+            _loggerHelper = loggerHelper;
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(LoginModel model)
+        {
+            if (!ModelState.IsValid) return BadRequest(ModelState);
+            var response = await _auth.Login(model);
+            if (!response.Status)
+                return BadRequest(response.Message);
+            var roleUser = await _auth.GetRoleByEmail(model.Email);
+            var token = _jwtHelper.GenerateToken(model.Email,roleUser.Data);
+            return Ok(response);
+        }
+
+    }
+}
--- Presentation/PM.API/Controllers/AuthController.cs
+++ Presentation/PM.API/Controllers/AuthController.cs
@@ -4,40 +4,26 @@ using System.Net.Http.Json;
 
 namespace PM.API.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/[controller]/[action]")]
     [ApiController]
     public class AuthController : ControllerBase
     {
         private readonly HttpClient _httpClient;
-        private string _gatewayUrl = "http://localhost:5000/auth/"; // URL của Ocelot Gateway
-
         public AuthController(HttpClient httpClient)
         {
             _httpClient = httpClient;
-            _httpClient.BaseAddress = new Uri(_gatewayUrl);
         }
-
-        [HttpPost("login")]
+        [HttpPost]
         public async Task<IActionResult> Login(string email, string password)
         {
-            var model = new LoginModel
+            var model = new LoginModel()
             {
                 Email = email,
                 Password = password
             };
-
-            // Gửi yêu cầu đến Ocelot Gateway
-            var response = await _httpClient.PostAsJsonAsync("login", model);
-
-            // Kiểm tra trạng thái phản hồi
-            if (!response.IsSuccessStatusCode)
-            {
-                var error = await response.Content.ReadAsStringAsync();
-                return StatusCode((int)response.StatusCode, new { Error = error });
-            }
-
-            var result = await response.Content.ReadAsStringAsync();
-            return Ok(new { Message = "Login successful", Data = result });
+            _httpClient.BaseAddress = new Uri("http://localhost/5000/auth");
+            var response = await _httpClient.PostAsJsonAsync<LoginModel>("login",model);
+            return Ok(response.Content);
         }
     }
 }
