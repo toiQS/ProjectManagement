bc886074fd1d7a27ba9705d431afb25931e7b558
Akai <nguyensieu12112002@gmail.com>
2025-01-11 17:17:10 +0700

    Create UserOverView.cs

---
+++ Domain/PM.DomainServices/Models/users/UserOverView.cs
@@ -0,0 +1,9 @@
+﻿namespace PM.DomainServices.Models.users
+{
+    public class UserOverView
+    {
+        public string ProjectName { get; set; } = string.Empty;
+        public string RoleName {  get; set; } = string.Empty;
+        public string PositionWorkName { get; set; } = string.Empty;
+    }
+}
