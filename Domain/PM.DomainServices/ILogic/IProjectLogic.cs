b46b9957f26d2b86bdf39e67f60fa98af738bfd4
Akai <nguyensieu12112002@gmail.com>
2025-01-12 17:40:10 +0700

    Create IProjectLogic.cs

---
+++ Domain/PM.DomainServices/ILogic/IProjectLogic.cs
@@ -0,0 +1,15 @@
+﻿using PM.DomainServices.Models.projects;
+using PM.DomainServices.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PM.DomainServices.ILogic
+{
+    public interface IProjectLogic
+    {
+        Task<ServicesResult<IEnumerable<IndexProject>>> GetIndexProjects();
+    }
+}
