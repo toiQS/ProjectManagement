d67f6e31ae857e64353870759e91354da04e8d30
Akai <nguyensieu12112002@gmail.com>
2025-01-12 17:40:04 +0700

    Create IMemberLogic.cs

---
+++ Domain/PM.DomainServices/ILogic/IMemberLogic.cs
@@ -0,0 +1,18 @@
+﻿using PM.DomainServices.Models;
+using PM.DomainServices.Models.members;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PM.DomainServices.ILogic
+{
+    public interface IMemberLogic
+    {
+        Task<ServicesResult<string>> GetRoleMemberInProject(string memberId);
+        Task<ServicesResult<IndexMember>> GetInfoOfOwnerInProject(string projectId); 
+        Task<ServicesResult<IEnumerable<IndexMember>>> GetAll();
+
+    }
+}
