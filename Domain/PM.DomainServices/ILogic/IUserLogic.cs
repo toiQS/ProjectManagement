71b8c359570e841b027aaed1adb6515873e1430a
Akai <nguyensieu12112002@gmail.com>
2025-01-12 08:16:16 +0700

    Create IUserLogic.cs

---
+++ Domain/PM.DomainServices/ILogic/IUserLogic.cs
@@ -0,0 +1,15 @@
+﻿using PM.DomainServices.Models.users;
+using PM.DomainServices.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PM.DomainServices.ILogic
+{
+    public interface IUserLogic
+    {
+        Task<ServicesResult<DetailAppUser>> GetInfoOtherUserByUserId(string userId);
+    }
+}
