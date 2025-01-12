5db99d0125d4a7077d16949c491b22542f43e42d
Akai <nguyensieu12112002@gmail.com>
2025-01-12 21:24:52 +0700

    Create TaskLogic.cs

---
+++ Domain/PM.DomainServices/Logic/TaskLogic.cs
@@ -0,0 +1,12 @@
+﻿using PM.Domain;
+using PM.DomainServices.Models;
+using PM.DomainServices.Models.plans;
+using PM.Persistence.IServices;
+
+namespace PM.DomainServices.Logic
+{
+    public class TaskLogic
+    {
+        
+    }
+}
