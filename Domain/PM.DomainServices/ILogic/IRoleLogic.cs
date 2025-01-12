2cea5343a131752a708881766beeabc620874227
Akai <nguyensieu12112002@gmail.com>
2025-01-12 17:40:13 +0700

    Create IRoleLogic.cs

---
+++ Domain/PM.DomainServices/ILogic/IRoleLogic.cs
@@ -0,0 +1,13 @@
+﻿using PM.Domain;
+using PM.DomainServices.Models;
+
+namespace PM.DomainServices.ILogic
+{
+    public interface IRoleLogic
+    {
+        public Task<ServicesResult<RoleInProject>> GetOwnerRole();
+        public Task<ServicesResult<RoleInProject>> GetLeaderRole();
+        public Task<ServicesResult<RoleInProject>> GetManagerRole();
+        public Task<ServicesResult<RoleInProject>> GetInfoRole(string roleId);
+    }
+}
