4da99907eaeb55231074babee4e3b0858878a0fb
Akai <nguyensieu12112002@gmail.com>
2025-01-13 09:47:31 +0700

    Update UserLogic.cs

--- Domain/PM.DomainServices/Logic/UserLogic.cs
+++ Domain/PM.DomainServices/Logic/UserLogic.cs
@@ -1,7 +1,10 @@
-﻿using PM.DomainServices.ILogic;
+﻿using Microsoft.AspNetCore.Identity;
+using PM.DomainServices.ILogic;
 using PM.DomainServices.Models;
+using PM.DomainServices.Models.projects;
 using PM.DomainServices.Models.users;
 using PM.Persistence.IServices;
+using System.Runtime.InteropServices;
 
 namespace PM.DomainServices.Logic
 {
@@ -10,16 +13,23 @@ namespace PM.DomainServices.Logic
         //intialize services
         private IApplicationUserServices _applicationUserServices;
         //intialize logic
-
+        private readonly IMemberLogic _memberLogic;
+        private readonly IProjectLogic _projectLogic;
         //intialize primary value
         private string _userIdCurrent = string.Empty;
         private DetailAppUser _userCurrent = new DetailAppUser();
 
 
         #region private method
-        
+
         #endregion
         #region suport method
+        #region
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <returns></returns>
         public async Task<ServicesResult<DetailAppUser>> GetInfoOtherUserByUserId(string userId)
         {
             if (string.IsNullOrEmpty(userId))
@@ -28,8 +38,6 @@ namespace PM.DomainServices.Logic
             var getUser = await _applicationUserServices.GetAppUserByIdOrEmail(userId);
             if (!getUser.Status || getUser.Data == null)
                 return ServicesResult<DetailAppUser>.Failure(getUser.Message);
-
-            // Check if user data is available
 
 
             // Map user data to ApplicationUser object
@@ -46,22 +54,21 @@ namespace PM.DomainServices.Logic
             return ServicesResult<DetailAppUser>.Success(userOther, string.Empty);
         }
         #endregion
+        #endregion
         #region primary method
 
-        
+
         public async Task<ServicesResult<DetailAppUser>> GetInfoUserCurrent(string userCurrentId)
         {
             if (string.IsNullOrEmpty(userCurrentId))
                 return ServicesResult<DetailAppUser>.Failure("User ID is required.");
-            _userIdCurrent = userCurrentId;
             // Attempt to fetch the user by ID or email
             var getUser = await _applicationUserServices.GetAppUserByIdOrEmail(_userIdCurrent);
-            if (!getUser.Status)
+            if (!getUser.Status || getUser.Data == null)
                 return ServicesResult<DetailAppUser>.Failure(getUser.Message);
 
-            // Check if user data is available
-            if (getUser.Data == null)
-                return ServicesResult<DetailAppUser>.Success(new DetailAppUser(), "Cannot retrieve user information.");
+          
+            _userIdCurrent = userCurrentId;
 
             // Map user data to ApplicationUser object
             _userCurrent = new DetailAppUser
@@ -76,7 +83,20 @@ namespace PM.DomainServices.Logic
 
             return ServicesResult<DetailAppUser>.Success(_userCurrent, string.Empty);
         }
-
+        public async Task<ServicesResult<IEnumerable<IndexProject>>> GetListProjectsUserHasJoined()
+        {
+           var projects = await _projectLogic.GetProjectsUserHasJoined(_userIdCurrent);
+           if(projects.Status == false) return ServicesResult<IEnumerable<IndexProject>>.Failure(projects.Message);
+            if (projects.Data == null) return ServicesResult<IEnumerable<IndexProject>>.Success(projects.Data, projects.Message);
+            return ServicesResult<IEnumerable<IndexProject>>.Success(projects.Data, string.Empty);
+        }
+        public async Task<ServicesResult<IEnumerable<IndexProject>>> GetListProjectsUserHasOwn()
+        {
+           var projects = await _projectLogic.GetProjectsUserHasOwn(_userIdCurrent);
+           if(projects.Status == false) return ServicesResult<IEnumerable<IndexProject>>.Failure(projects.Message);
+            if (projects.Data == null) return ServicesResult<IEnumerable<IndexProject>>.Success(projects.Data, projects.Message);
+            return ServicesResult<IEnumerable<IndexProject>>.Success(projects.Data, string.Empty);
+        }
 
         #endregion
 
