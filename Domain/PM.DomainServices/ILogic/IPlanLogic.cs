4c4aeae855bd9a9b4a0e369183caebe0bf414a06
Akai <nguyensieu12112002@gmail.com>
2025-01-13 07:59:31 +0700

    Create IPlanLogic.cs

---
+++ Domain/PM.DomainServices/ILogic/IPlanLogic.cs
@@ -0,0 +1,15 @@
+﻿using PM.DomainServices.Models.plans;
+using PM.DomainServices.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PM.DomainServices.ILogic
+{
+    public interface IPlanLogic
+    {
+        Task<ServicesResult<IEnumerable<IndexPlan>>> GetPlansInProject(string projectId);
+    }
+}
