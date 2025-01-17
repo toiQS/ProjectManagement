5f5b2db6f71eb20feb0674fd2110c3dbfc90e5ed
Akai <nguyensieu12112002@gmail.com>
2025-01-17 15:08:54 +0700

    Update IUserLogic.cs

--- Domain/PM.DomainServices/ILogic/IUserLogic.cs
+++ Domain/PM.DomainServices/ILogic/IUserLogic.cs
@@ -1,4 +1,5 @@
 ﻿using PM.DomainServices.Models;
+using PM.DomainServices.Models.projects;
 using PM.DomainServices.Models.users;
 
 namespace PM.DomainServices.ILogic
@@ -7,5 +8,7 @@ namespace PM.DomainServices.ILogic
     {
         Task<ServicesResult<DetailAppUser>> GetInfoOtherUserByUserId(string userId);
         Task<ServicesResult<DetailAppUser>> GetInfoUserCurrent(string userCurrentId);
+        Task<ServicesResult<IEnumerable<IndexProject>>> GetListProjectsUserHasJoined();
+        Task<ServicesResult<IEnumerable<IndexProject>>> GetListProjectsUserHasOwn();
     }
 }
