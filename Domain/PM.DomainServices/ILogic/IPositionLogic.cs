4be2ae2b103512a1202d9fd104237743410c2855
Akai <nguyensieu12112002@gmail.com>
2025-01-17 15:08:52 +0700

    Update IPositionLogic.cs

--- Domain/PM.DomainServices/ILogic/IPositionLogic.cs
+++ Domain/PM.DomainServices/ILogic/IPositionLogic.cs
@@ -1,10 +1,12 @@
-﻿using PM.DomainServices.Models;
+﻿using PM.Domain;
+using PM.DomainServices.Models;
 
 namespace PM.DomainServices.ILogic
 {
     public interface IPositionLogic
     {
         Task<ServicesResult<string>> GetPositionWorkByMemberId(string memberId);
+        Task<ServicesResult<PositionWorkOfMember>> GetPositionWorkOfMember(string memberId);
 
     }
 }
