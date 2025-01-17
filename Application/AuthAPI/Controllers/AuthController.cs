586f78e99a0e6d62998f631b7a7be8c704c88b01
Akai <nguyensieu12112002@gmail.com>
2025-01-17 19:08:01 +0700

    Create AuthController.cs

---
+++ Application/AuthAPI/Controllers/AuthController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PM.DomainServices.Logic;
+using PM.DomainServices.Models;
+using PM.DomainServices.Models.auths;
+using PM.Infrastructure.jwt;
+
+namespace AuthAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IJwtHelper _jwtHelper;
+        private readonly AuthLogic _authLogic;
+        public AuthController(IJwtHelper jwtHelper, AuthLogic authLogic)
+        {
+            _jwtHelper = jwtHelper;
+            _authLogic = authLogic;
+        }
+        [HttpPost]
+        public async Task<ServicesResult<string>> Login(LoginModel loginModel)
+        {
+            var login = await _authLogic.Login(loginModel);
+            if(login.Status == false) return ServicesResult<string>.Failure(login.Message);
+        }
+    }
+}
