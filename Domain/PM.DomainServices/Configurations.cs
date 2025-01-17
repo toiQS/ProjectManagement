38d847231d7d57e60cca631b50473fd65de6f8d7
Akai <nguyensieu12112002@gmail.com>
2025-01-17 15:14:09 +0700

    Update Configurations.cs

--- Domain/PM.DomainServices/Configurations.cs
+++ Domain/PM.DomainServices/Configurations.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using PM.DomainServices.ILogic;
+using PM.DomainServices.Logic;
 using PM.Persistence.IServices;
 using PM.Persistence.Services;
 
@@ -14,12 +16,12 @@ namespace PM.DomainServices
         }
         private static void RegisterLogic(IServiceCollection services)
         {
-            //services.AddScoped<IAuthLogic, AuthLogic>();
-            //services.AddScoped<IMemberLogic, MemberLogic>();
-            //services.AddScoped<IPlanLogic, PlanLogic>();
-            //services.AddScoped<IPositionLogic, PositionLogic>();
-            //services.AddScoped<IProjectLogic, ProjectLogic>();
-            //services.AddScoped<ITaskLogic, TaskLogic>();
+            services.AddScoped<IMemberLogic, MemberLogic>();
+            services.AddScoped<IPlanLogic, PlanLogic>();
+            services.AddScoped<IPositionLogic, PositionLogic>();
+            services.AddScoped<IProjectLogic, ProjectLogic>();
+            services.AddScoped<IRoleLogic, RoleLogic>();
+            services.AddScoped<IUserLogic, UserLogic>();
         }
         private static void RegisterServices(IServiceCollection services)
         {
