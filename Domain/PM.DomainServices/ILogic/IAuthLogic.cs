80917fda3a05826176463723d178a437f8e33eca
Akai <nguyensieu12112002@gmail.com>
2025-01-25 20:13:14 +0700

    Create IAuthLogic.cs

---
+++ Domain/PM.DomainServices/ILogic/IAuthLogic.cs
@@ -0,0 +1,16 @@
+﻿using PM.DomainServices.Models.auths;
+using PM.DomainServices.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PM.DomainServices.ILogic
+{
+    public interface IAuthLogic
+    {
+        Task<ServicesResult<AppUserClaim>> Login(LoginModel loginModel);
+        Task<ServicesResult<bool>> Register(RegisterModel registerModel);
+    }
+}
