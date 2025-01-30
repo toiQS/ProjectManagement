eb19ec20b5a8ce4563ae4f21e822acbe0ebef8b4
Akai <nguyensieu12112002@gmail.com>
2025-01-30 23:18:00 +0700

    Update Configurations.cs

--- Domain/PM.DomainServices/Configurations.cs
+++ Domain/PM.DomainServices/Configurations.cs
@@ -1,11 +1,5 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using PM.DomainServices.ILogic;
-using PM.DomainServices.IServices;
-using PM.DomainServices.Logic;
-using PM.DomainServices.Services;
-using PM.Persistence.IServices;
-using PM.Persistence.Services;
 
 namespace PM.DomainServices
 {
@@ -18,24 +12,11 @@ namespace PM.DomainServices
         }
         private static void RegisterLogic(IServiceCollection services)
         {
-            services.AddScoped<IUserLogic, UserLogic>();
-            services.AddScoped<IAuthLogic, AuthLogic>();
+            
         }
         private static void RegisterServices(IServiceCollection services)
         {
-            services.AddScoped<IApplicationUserServices, ApplicationUserServices>();
-            services.AddScoped<IAuthServices, AuthServices>();
-            services.AddScoped<IMemberInTaskServices, MemberInTaskServices>();
-            services.AddScoped<IPlanInProjectServices, PlanInProjectServices>();
-            services.AddScoped<IPlanServices, PlanServices>();
-            services.AddScoped<IPositionInProjectServices, PositionInProjectServices>();
-            services.AddScoped<IPositionWorkOfMemberServices, PositionWorkOfMemberServices>();
-            services.AddScoped<IProjectServices, ProjectServices>();
-            services.AddScoped<IRoleApplicationUserInProjectServices, RoleApplicationUserInProjectServices>();
-            services.AddScoped<IRoleInProjectServices, RoleInProjectServices>();
-            services.AddScoped<ITaskInPlanServices, TaskInPlanServices>();
-            services.AddScoped<ITaskServices, TaskServices>();
-            services.AddScoped<IStatusServices, StatusServices>();
+            
         }
     }
 }
