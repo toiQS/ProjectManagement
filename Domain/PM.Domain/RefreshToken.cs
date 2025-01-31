0fa4fd07ea2e4dc56b7f4ad20f43f7745469b5f1
Akai <nguyensieu12112002@gmail.com>
2025-01-31 19:27:08 +0700

    Create RefreshToken.cs

---
+++ Domain/PM.Domain/RefreshToken.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PM.Domain
+{
+    public class RefreshToken
+    {
+        public int Id { get; set; }
+        public string Token { get; set; } = string.Empty;
+        [ForeignKey(nameof(User))]
+        public string UserId { get; set; } = string.Empty;
+        public ApplicationUser User { get; set; }
+        public DateTime ExpiryDate { get; set; }
+        public bool IsRevoked { get; set; }
+    }
+
+}
