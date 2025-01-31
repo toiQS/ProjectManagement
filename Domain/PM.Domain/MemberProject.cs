0e36c254ca556c98d00fe87365dd369c7e49a25b
Akai <nguyensieu12112002@gmail.com>
2025-01-31 19:26:59 +0700

    Create MemberProject.cs

---
+++ Domain/PM.Domain/MemberProject.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PM.Domain
+{
+    [Table(name: "Member In Project")]
+    public class MemberProject
+    {
+        [Key]
+        public string Id { get; set; }
+       
+        
+        [ForeignKey(nameof(RoleInProject))]
+        [Column(name: "Role In Project Id")]
+        public string RoleInProjectId { get; set; } = string.Empty;
+        public RoleInProject RoleInProject { get; set; }
+        [ForeignKey(nameof(ApplicationUser))]
+        [Column(name: "Application User Id")]
+        public string ApplicationUserId { get; set; } = string.Empty;
+        public ApplicationUser ApplicationUser { get; set; }
+        [ForeignKey(nameof(PositionInProject))]
+        public string PositionInProjectId {  get; set; } = string.Empty;
+        public PositionInProject PositionInProject {  get; set; }
+    }
+}
