5c7a40c9a2788497d6704c4ad29a0851c29ff908
Akai <nguyensieu12112002@gmail.com>
2025-01-31 19:30:47 +0700

    Create UserLogic.cs

---
+++ Domain/PM.DomainServices/Logics/UserLogic.cs
@@ -0,0 +1,11 @@
+﻿using PM.Domain;
+using PM.DomainServices.Repository;
+
+namespace PM.DomainServices.Logics
+{
+    public class UserLogic
+    {
+        private readonly IRepository<ApplicationUser> _repository;
+
+    }
+}
