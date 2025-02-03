71fa15f41bb7163a1313cee2df536095e21dbb82
Akai <nguyensieu12112002@gmail.com>
2025-02-03 09:00:01 +0700

    Update IProjectLogic.cs

--- Domain/PM.DomainServices/ILogics/IProjectLogic.cs
+++ Domain/PM.DomainServices/ILogics/IProjectLogic.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using PM.DomainServices.Models.projects;
+using PM.DomainServices.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,6 @@ namespace PM.DomainServices.ILogics
 {
     public interface IProjectLogic
     {
+        Task<ServicesResult<IEnumerable<IndexProject>>> GetProjectListUserHasJoined(string userId);
     }
 }
