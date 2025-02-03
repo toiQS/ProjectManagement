46b685ab80c90f26bc4e9c600617d85cf766639d
Akai <nguyensieu12112002@gmail.com>
2025-02-03 09:00:04 +0700

    Update IUserLogic.cs

--- Domain/PM.DomainServices/ILogics/IUserLogic.cs
+++ Domain/PM.DomainServices/ILogics/IUserLogic.cs
@@ -1,16 +1,11 @@
-﻿using PM.DomainServices.Models.users;
-using PM.DomainServices.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using PM.DomainServices.Models;
+using PM.DomainServices.Models.users;
 
 namespace PM.DomainServices.ILogics
 {
-    public interface IUserLogic : IDisposable
+    public interface IUserLogic
     {
         Task<ServicesResult<DetailAppUser>> DetailUserCurrent(string userId);
-        Task<ServicesResult<DetailAppUser>> UpdateInfo(string userId, DetailAppUser newInfo);
+        Task<ServicesResult<DetailAppUser>> UpdateInfo(string userId, UpdateAppUser newInfo);
     }
 }
