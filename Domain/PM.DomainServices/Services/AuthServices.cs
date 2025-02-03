cbbc07a9ae1e09b118433e262b44cd1b686eb051
Akai <nguyensieu12112002@gmail.com>
2025-02-03 23:47:01 +0700

    Create AuthServices.cs

---
+++ Domain/PM.DomainServices/Services/AuthServices.cs
@@ -0,0 +1,21 @@
+﻿using PM.DomainServices.ILogics;
+using PM.DomainServices.Models;
+using PM.DomainServices.Models.auths;
+using PM.Infrastructure.jwt;
+
+namespace PM.DomainServices.Services
+{
+    public class AuthServices
+    {
+        private readonly IAuthLogic _auth;
+        private readonly IJwtHelper _jwtHelper;
+        public async Task<ServicesResult<string>> Login(LoginModel loginModel)
+        {
+            if (loginModel == null) return ServicesResult<string>.Failure("email or password is invalid");
+            var login = await _auth.LoginAsync(loginModel);
+            if(login.Status == false) return ServicesResult<string>.Failure(login.Message);
+            var tokne = _jwtHelper.GenerateTokenString(loginModel.Email, "gaf0");
+            return ServicesResult<string>.Success(tokne,string.Empty);
+        }
+    }
+}
