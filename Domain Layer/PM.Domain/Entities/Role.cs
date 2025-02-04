8503bee5caf07fdcbabb4d98bb7381a684e474ba
Akai <nguyensieu12112002@gmail.com>
2025-02-04 16:10:43 +0700

    Create Role.cs

---
+++ Domain Layer/PM.Domain/Entities/Role.cs
@@ -0,0 +1,11 @@
+﻿namespace PM.Domain.Entities
+{
+    public class Role
+    {
+        public string Id { get; set; } // Mã vai trò
+        public string Name { get; set; } // Tên vai trò
+        public string Description { get; set; } // Mô tả vai trò
+
+        public ICollection<ProjectMember> ProjectMembers { get; set; } // Thành viên có vai trò này
+    }
+}
