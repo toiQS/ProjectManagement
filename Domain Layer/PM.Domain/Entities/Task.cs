6a8665a332bc5a2aba7d53703fdce9890a3ed794
Akai <nguyensieu12112002@gmail.com>
2025-02-04 16:11:29 +0700

    Create Task.cs

---
+++ Domain Layer/PM.Domain/Entities/Task.cs
@@ -0,0 +1,18 @@
+﻿namespace PM.Domain.Entities
+{
+    public class Task
+    {
+        public string Id { get; set; } // Mã nhiệm vụ
+        public string PlanId { get; set; } // Mã kế hoạch
+        public string Name { get; set; } // Tên nhiệm vụ
+        public string Description { get; set; } // Mô tả nhiệm vụ
+        public DateTime StartDate { get; set; } // Ngày bắt đầu
+        public DateTime EndDate { get; set; } // Ngày kết thúc
+        public int StatusId { get; set; } // ID tình trạng
+        public bool IsCompleted { get; set; } // Đã hoàn thành chưa
+
+        public Plan Plan { get; set; } // Liên kết đến kế hoạch
+        public Status Status { get; set; } // Liên kết đến tình trạng
+        public ICollection<TaskAssignment> Assignments { get; set; } // Giao nhiệm vụ
+    }
+}
