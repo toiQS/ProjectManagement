b8c8ac97aa945f117c148de3a601aaeb88767559
Akai <nguyensieu12112002@gmail.com>
2025-02-04 16:11:31 +0700

    Create TaskAssignment.cs

---
+++ Domain Layer/PM.Domain/Entities/TaskAssignment.cs
@@ -0,0 +1,12 @@
+﻿namespace PM.Domain.Entities
+{
+    public class TaskAssignment
+    {
+        public string Id { get; set; } // Mã gán nhiệm vụ
+        public string TaskId { get; set; } // Mã nhiệm vụ
+        public string ProjectMemberId { get; set; } // Mã thành viên dự án
+
+        public Task Task { get; set; } // Liên kết đến nhiệm vụ
+        public ProjectMember ProjectMember { get; set; } // Liên kết đến thành viên dự án
+    }
+}
