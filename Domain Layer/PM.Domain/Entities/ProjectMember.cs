744fa23b47143db8ddc0abf0c05313c344ca9eb8
Akai <nguyensieu12112002@gmail.com>
2025-02-04 16:10:37 +0700

    Create ProjectMember.cs

---
+++ Domain Layer/PM.Domain/Entities/ProjectMember.cs
@@ -0,0 +1,14 @@
+﻿namespace PM.Domain.Entities
+{
+    public class ProjectMember
+    {
+        public string Id { get; set; } // Mã thành viên dự án
+        public string ProjectId { get; set; } // Mã dự án
+        public string UserId { get; set; } // Mã người dùng
+        public string RoleId { get; set; } // Vai trò trong dự án
+
+        public Project Project { get; set; } // Liên kết đến dự án
+        public User User { get; set; } // Liên kết đến người dùng
+        public Role Role { get; set; } // Liên kết đến vai trò
+    }
+}
