f3a9e431a6c2c173557ba8dc441e732b3281da98
Akai <nguyensieu12112002@gmail.com>
2025-02-04 16:10:40 +0700

    Create RefreshToken.cs

---
+++ Domain Layer/PM.Domain/Entities/RefreshToken.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PM.Domain.Entities
+{
+    public class RefreshToken
+    {
+        public string Id { get; set; }
+        public string Token { get; set; }
+        public DateTime Expires { get; set; }
+        public bool IsExpired => DateTime.UtcNow >= Expires;
+        public DateTime Created { get; set; }
+        public DateTime? Revoked { get; set; }
+        public bool IsActive => Revoked == null && !IsExpired;
+
+        public string UserId { get; set; }
+        public User User { get; set; }
+    }
+
+}
