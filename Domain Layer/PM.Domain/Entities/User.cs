bd08479503b30a110eb310621e8dfcecc183c690
Akai <nguyensieu12112002@gmail.com>
2025-02-04 16:11:33 +0700

    Create User.cs

---
+++ Domain Layer/PM.Domain/Entities/User.cs
@@ -0,0 +1,16 @@
+﻿namespace PM.Domain.Entities
+{
+    public class User
+    {
+        public string Id { get; set; } // Mã người dùng
+        public string FirstName { get; set; } // Họ
+        public string LastName { get; set; } // Tên
+        public string Nickname { get; set; } // Biệt danh
+        public string Email { get; set; } // Email
+        public string PhoneNumber { get; set; } // Số điện thoại
+        public string AvatarPath { get; set; } // Đường dẫn ảnh đại diện
+
+        public ICollection<ProjectMember> ProjectMemberships { get; set; } // Các dự án tham gia
+        public List<RefreshToken> RefreshTokens { get; set; } = new();
+    }
+}
