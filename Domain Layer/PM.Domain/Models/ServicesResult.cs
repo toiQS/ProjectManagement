0f2a2930f3c3d799870545ea6d2304c67abf8070
Akai <nguyensieu12112002@gmail.com>
2025-02-04 16:11:46 +0700

    Create ServicesResult.cs

---
+++ Domain Layer/PM.Domain/Models/ServicesResult.cs
@@ -0,0 +1,22 @@
+﻿namespace PM.Domain.Models
+{
+    public class ServicesResult<T>
+    {
+        private T _value;
+        private string _message;
+        private bool _status;
+        public ServicesResult(T value)
+        {
+            if (value is null)
+            {
+                _message = "No data here";
+                _status = true;
+            }
+            else
+            {
+                _message = "Success";
+
+            }
+        }
+    }
+}
