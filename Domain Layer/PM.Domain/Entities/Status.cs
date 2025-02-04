66e38ef97b8b8196ecde6473fbe13e59cb5324e9
Akai <nguyensieu12112002@gmail.com>
2025-02-04 16:10:46 +0700

    Create Status.cs

---
+++ Domain Layer/PM.Domain/Entities/Status.cs
@@ -0,0 +1,12 @@
+﻿namespace PM.Domain.Entities
+{
+    public class Status
+    {
+        public int Id { get; set; } // Mã tình trạng
+        public string Name { get; set; } // Tên tình trạng
+
+        public ICollection<Project> Projects { get; set; } // Dự án có tình trạng này
+        public ICollection<Plan> Plans { get; set; } // Kế hoạch có tình trạng này
+        public ICollection<Task> Tasks { get; set; } // Nhiệm vụ có tình trạng này
+    }
+}
